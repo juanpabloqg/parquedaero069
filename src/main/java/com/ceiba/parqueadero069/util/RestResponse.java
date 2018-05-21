25d7d9a1ae3b9fdf1ef17cf402c0f937ee60e8ad
Juan Pablo Quiñones Giraldo <jpqg07@gmail.com>
2018-05-21 11:18:32 -0500

    Se elimina constructor sin uso.

--- src/main/java/com/ceiba/parqueadero069/util/RestResponse.java
+++ src/main/java/com/ceiba/parqueadero069/util/RestResponse.java
@@ -3,16 +3,10 @@ package com.ceiba.parqueadero069.util;
 public class RestResponse {
 	
 	private int responseCode;
-	private String message;
+	private String message;	
 	
-	
-	
-	public RestResponse(int responseCode) {
-		super();
-		this.responseCode = responseCode;
-	}
 	public RestResponse(int responseCode, String message) {
-		super();
+		
 		this.responseCode = responseCode;
 		this.message = message;
 	}
